5d8f595abf342202e7071cb8741e9df00b8b2884
ankit-1k <ankitpanda922@gmail.com>
2024-11-10 12:15:59 +0530

    patch cancel now adding report on full data on report.jsx

--- src/Admin/Reports/Report.jsx
+++ src/Admin/Reports/Report.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
 import "./report.css";
 import AdHeader from "../header/AdHeader";
@@ -9,13 +8,16 @@ import YearlyReport from "./yearly/YearlyReport";
 import reportImg from "./../../assets/img/reports.gif";
 
 const Report = () => {
+  const [visible, setVisible] = useState(false);
 
   return (
     <>
       <AdHeader />
       <div className="container">
         <p
-          className="mt-3 lead fw-bold text-end pointer info-text">
+          className="mt-3 lead fw-bold text-end pointer info-text"
+          onClick={() => setVisible(true)}
+        >
           Booking Sales Report Info{" "}
           <i className="pi pi-info-circle text-info"></i>
         </p>
@@ -24,18 +26,31 @@ const Report = () => {
             <img src={reportImg} alt="" className="reportImg" />
           </div>
           <div className="col-md-6">
+            <h5 className="text-muted">Sales Weekly Reports :</h5>
             <WeeklyReports />
           </div>
         </div>
         <div className="row mt-5">
           <div className="col-md-6">
+            <h5 className="text-muted">Sales Monthly Report :</h5>
             <MonthlyReports />
           </div>
           <div className="col-md-6">
+            <h5 className="text-muted">Sales Yearly Report :</h5>
             <YearlyReport />
           </div>
         </div>
       </div>
+      <Dialog
+        header="Header"
+        visible={visible}
+        maximizable
+        style={{ width: "50vw" }}
+        onHide={() => {
+          if (!visible) return;
+          setVisible(false);
+        }}
+      ></Dialog>
     </>
   );
 };
--- src/Admin/Reports/WeeklyReports/WeeklyReports.jsx
+++ src/Admin/Reports/WeeklyReports/WeeklyReports.jsx
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Chart } from "primereact/chart";
 import axios from "axios";
 import moment from "moment";
-import { Dialog } from "primereact/dialog";
 
 const WeeklyReports = () => {
   const [chartData, setChartData] = useState({});
   const [chartOptions, setChartOptions] = useState({});
   const [reservations, setReservations] = useState([]);
-  const [visible, setVisible] = useState(false);
+
   useEffect(() => {
     fetchReservations();
   }, []);
@@ -20,7 +19,6 @@ const WeeklyReports = () => {
       );
       setReservations(response.data);
       processReservationsData(response.data);
-      setReservations(response)
     } catch (error) {
       console.error("Error fetching reservations", error);
     }
@@ -51,10 +49,9 @@ const WeeklyReports = () => {
       });
     });
 
+    // Prepare data for the chart
     const labels = Object.keys(weeksData);
     const values = Object.values(weeksData);
-
-    console.log(reservations)
 
     const dataForChart = {
       labels: labels,
@@ -86,26 +83,9 @@ const WeeklyReports = () => {
 
   return (
     <>
-      <p
-        className="mt-3 lead fw-bold pointer info-text m-0 "
-        onClick={() => setVisible(true)}
-      >
-        Sales Weekly Reports <i className="pi pi-info-circle text-info"></i>
-      </p>
       <div className="card">
         <Chart type="bar" data={chartData} options={chartOptions} />
       </div>
-
-      <Dialog
-        header="Sales Weekly Reports"
-        visible={visible}
-        maximizable
-        style={{ width: "50vw" }}
-        onHide={() => {
-          if (!visible) return;
-          setVisible(false);
-        }}
-      ></Dialog>
     </>
   );
 };
